df4a3121a717b32b588b45cd251f75bafc49fcbf
vvmk <vincentmasiello@gmail.com>
2018-09-09 16:33:54 -0400

    Use Route Model Binding in TasksController

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -13,9 +13,22 @@ class TasksController extends Controller
         return view('tasks.index', compact('tasks'));
     }
 
-    public function show($id) {
-        $task = Task::find($id);
+    /* public function show($id) { */
+    /*     $task = Task::find($id); */
 
+    /*     return view('tasks.show', compact('task')); */
+    /* } */
+
+    /*
+     * Route Model Binding
+     *
+     * This replaces the above method. By default laravel looks for 
+     * a primary key, but can be configured to use a slug or whatever.
+     *
+     * It is important that the prop name is $task, like as it was
+     * typed in the route: '/tasks/{task}'
+     */
+    public function show(Task $task) {
         return view('tasks.show', compact('task'));
     }
 }
