459d910f7a1aa314b059f26abb04c29e9a161fad
vvmk <vincentmasiello@gmail.com>
2018-09-09 18:36:51 -0400

    Add a couple style classes for readability

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,8 +1,9 @@
 @extends ('layouts.master')
-
 @section ('content')
-    <h1>
+<div class="h-100 p-2 container">
+    <h1 class="mb-2 text-center">
         {{ $post->title }}
     </h1>
-    <p>{{ $post->body }}</p>
+    <p class="p-2">{{ $post->body }}</p>
+</div>
 @endsection
