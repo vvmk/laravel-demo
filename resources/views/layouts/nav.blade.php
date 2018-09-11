3e80b57d3689c015efcd167a9e158d3d03e4b8d4
vvmk <vincentmasiello@gmail.com>
2018-09-11 16:29:58 -0400

    Add logged-in user to nav menu

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,26 +5,28 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mr-auto w-100">
             <li class="nav-item active">
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/tasks">Tasks</a>
             </li>
-            <li class="nav-item dropdown">
+
+            @if (Auth::check())
+
+            <li class="ml-auto nav-item dropdown">
+                <a class="disabled" href="#">Welcome back, {{ Auth::user()->name }}</a>
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
                 <div class="dropdown-menu" aria-labelledby="dropdown01">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <a class="dropdown-item" href="/logout">Log out</a>
                 </div>
             </li>
+
+            @endif
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Find a post...</button>
-        </form>
     </div>
 </nav>
