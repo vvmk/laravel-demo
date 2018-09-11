0471033b7c3b35ab8f4a169181653406174a9c92
vvmk <vincentmasiello@gmail.com>
2018-09-11 16:19:34 -0400

    Add errors partial, pasword confirmation

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,21 +3,23 @@
 <form method="POST" action="/register">
     <div class="form-group">
         <label for="name">Name</label>
-        <input id="name" class="form-control" type="text" name="name">
+        <input id="name" class="form-control" type="text" name="name" required>
     </div>
     <div class="form-group">
         <label for="email">Email</label>
-        <input id="email" class="form-control" type="email" name="email">
+        <input id="email" class="form-control" type="email" name="email" required>
     </div>
     <div class="form-group">
         <label for="password">Password</label>
-        <input id="password" class="form-control" type="password" name="password">
+        <input id="password" class="form-control" type="password" name="password" required>
     </div>
     <div class="form-group">
         <label for="confirm">Password</label>
-        <input id="confirm" class="form-control" type="password" name="confirm">
+        <input id="confirm" class="form-control" type="password" name="confirm" required>
     </div>
     <button class="btn btn-success" type="submit">Sign up</button>
+
+    @include ('layouts.errors')
 
 </form>
 @endsection
