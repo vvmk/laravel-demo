fea511715ab4d2d19a7f20741bbc5b20626216fd
vvmk <vincentmasiello@gmail.com>
2018-09-11 18:03:41 -0400

    Require auth middleware in postsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,11 @@ use App\Post;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
         $posts = Post::all();
 
