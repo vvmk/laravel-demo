1ee0adaa68c92f0b15ba4441736981096a15ee1d
vvmk <vincentmasiello@gmail.com>
2018-09-11 23:17:57 -0400

    Display a posts comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,6 +4,22 @@
     <h1 class="mb-2 text-center">
         {{ $post->title }}
     </h1>
-    <p class="p-2">{{ $post->body }}</p>
+    <p class="p-2">
+    {{ $post->body }}
+    </p>
+
+    <hr>
+
+    <div class="comments">
+        <ul class="list-group">
+            @foreach ($post->comments as $comment)
+            <li class="list-group-item">
+                <strong>{{ $comment->created_at->diffForHumans() }}: &nbsp;</strong>
+                {{ $comment->body }}
+            </li>
+            @endforeach
+        </ul>
+    </div>
+
 </div>
 @endsection
