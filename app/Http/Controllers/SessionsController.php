2e0cf5044eed80029dbdb95ce9ba818333e8e6ed
vvmk <vincentmasiello@gmail.com>
2018-09-12 23:54:20 -0400

    Sign in page, sessions@store

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,12 +6,25 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
-    public function create() {
+    public function __construct() {
+        // auth'd users can't see this page.
+        $this->middleware('guest');
+    }
 
+    public function create() {
+        return view('sessions.create');
     }
 
     public function destroy() {
         auth()->logout();
+
+        return redirect()->home();
+    }
+
+    public function store(Request $request) {
+        if (!auth()->attempt(request(['email', 'password']))) {
+            return back();
+        }
 
         return redirect()->home();
     }
---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,19 @@
+@extends ('layouts.master')
+@section ('content')
+<div class="col-md-8">
+    <h1>Sign in</h1>
+    <form method="POST" action="/sessions"></form>
+    {{ csrf_field() }}
+    <div class="form-group">
+        <label for="email">Email Address</label>
+        <input id="email" class="form-control" type="text" name="email">
+    </div>
+    <div class="form-group">
+        <label for="password">Password</label>
+        <input id="password" class="form-control" type="password" name="password">
+    </div>
+    <div class="form-group">
+        <button class="btn btn-success" type="submit">Sign in</button>
+    </div>
+</div>
+@endsection
